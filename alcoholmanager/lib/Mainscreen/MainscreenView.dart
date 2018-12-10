236a048cb058717c7f0e7c79d4c2a93bfa0bafc0
Michael Trenker <michael.trenker@htlpinkafeld.at>
2018-12-10 19:08:31 +0100

    example pic appBar

--- alcoholmanager/lib/Mainscreen/MainscreenView.dart
+++ alcoholmanager/lib/Mainscreen/MainscreenView.dart
@@ -2,7 +2,7 @@ import 'package:alcoholmanager/AlcoholService.dart';
 import 'package:alcoholmanager/Mainscreen/Mainscreen.dart';
 import 'package:alcoholmanager/Mainscreen/Widget/DrinkCard.dart';
 import 'package:flutter/material.dart';
-
+import 'package:sliver_fab/sliver_fab.dart';
 class MainscreenView extends MainscreenState{
   @override
   Widget build(BuildContext context) {
@@ -11,11 +11,17 @@ class MainscreenView extends MainscreenState{
         
         
         body: CustomScrollView(
+          
           slivers: <Widget>[
+            
             SliverAppBar(
               expandedHeight: 200,
               flexibleSpace: FlexibleSpaceBar(
                 title: Text("data"),
+                background: Image.network(
+                  "https://gdb.voanews.com/94005114-AEB2-4C0B-AC85-FC2605E2B75E_cx0_cy3_cw0_w1023_r1_s.jpg",
+                  fit: BoxFit.cover,),
+
               ),
               floating: true,
 
@@ -39,8 +45,13 @@ class MainscreenView extends MainscreenState{
 
           
           
-        )
-        
+        ),
+        floatingActionButton: FloatingActionButton(
+          elevation: 0.0,
+        child: new Icon(Icons.add),
+          backgroundColor: new Color(0xFFE57373),
+          onPressed: (){}
+        ),
         
        
       ),
